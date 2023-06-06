9370b9baa08959d64393ed85bab05793830d185c
elliecw <hanchig@gmail.com>
2023-06-06 16:07:06 +0800

    feat: show comments on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,10 @@ const commentController = require('../controllers/comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
-const admin = require('./modules/admin') //新增這行，載入 admin.js
+const admin = require('./modules/admin') // 新增這行，載入 admin.js
 router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
-router.post('/signup', userController.signUp) //注意用 post
+router.post('/signup', userController.signUp) // 注意用 post
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/logout', userController.logout)
