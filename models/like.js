7f5a6ff52e0eb54d46e61f6e2c451c590a993ab0
elliecw <hanchig@gmail.com>
2023-06-13 00:41:19 +0800

    feat: add like/unlike button at index page

--- models/like.js
+++ models/like.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Like',
-    tableName: 'Like',
+    tableName: 'Likes',
     underscored: true
   })
   return Like
